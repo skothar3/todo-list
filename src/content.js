57f59c169de66bfd69f865b4a663ebc4307ec8cf
Sid Kothari <sid.kothari7@gmail.com>
2025-01-08 15:29:18 -0500

    Add interactive dialog form elements and Javascript for editing list items and creating new ones

--- src/content.js
+++ src/content.js
@@ -25,9 +25,40 @@ function displayController() {
   const footerEl = document.querySelector("footer");
 
   // PRIVATE METHODS {{{
+  const showItemDialog = (listItem) => {
+    const itemDialog = document.querySelector("dialog#item");
+    const formHeader = document.querySelector("h3#form-header");
+    if (listItem) {
+      formHeader.textContent = "EDIT ITEM";
+      const itemForm = document.querySelector("form");
+      const formElements = itemForm.elements;
+      itemForm.dataset.id = listItem.getID();
+      formElements["title"].value = listItem.title;
+      formElements["date"].value = format(listItem.date, "yyyy-MM-dd");
+      formElements["notes"].value = listItem.notes;
+      formElements[listItem.priority].checked = true;
+      formElements["completion"].checked = listItem.getCompletion();
+      formElements["tags"].value = listItem.tags.join(", ");
+    } else {
+      formHeader.textContent = "NEW ITEM";
+    }
+    itemDialog.showModal();
+  }
+
+  const closeItemDialog = () => {
+    const itemDialog = document.querySelector("dialog#item");
+    const itemForm = itemDialog.querySelector("form");
+    const itemFormData = new FormData(itemForm);
+    if (itemForm.dataset.id) {
+      itemFormData.append("id", itemForm.dataset.id);
+    }
+    itemDialog.close();
+    itemForm.reset();
+    return itemFormData;
+  }
+
   const initSortSelect = () => {
-    const selectContainer = document.createElement("div");
-    selectContainer.classList.add("select-container");
+    const contentToolbar = contentSection.querySelector("div#content-toolbar");
     
     const selectLabel = document.createElement("label");
     selectLabel.htmlFor = "sort-select";
@@ -43,9 +74,7 @@ function displayController() {
       selectEl.appendChild(option);
     });
 
-    selectContainer.append(selectLabel, selectEl);
-    contentSection.prepend(selectContainer);
-
+    contentToolbar.append(selectLabel, selectEl);
   }
   const createListItemDOM = (listItem) => {
     // {{{
@@ -92,6 +121,16 @@ function displayController() {
     return itemContainer;
   }; // }}}
 
+  // }}}
+
+  // PUBLIC METHODS {{{
+  const init = (listItemsArr, tags) => {
+    updateSidebar(tags);
+    updateList(listItemsArr);
+    initSortSelect();
+    insertFooter(footerEl);
+  };
+
   const updateSidebar = (todoListTags) => {
     // {{{
     const menuContainerDiv = document.createElement("div");
@@ -103,11 +142,9 @@ function displayController() {
     const projectsH2 = document.createElement("h2");
     projectsH2.textContent = "Projects";
 
-    const addItemBtn = document.createElement("button");
-    addItemBtn.id = "add-item";
-    addItemBtn.textContent = "+";
-
-    // projectsContainerDiv.appendChild(projectsH2);
+    const newItemBtn = document.createElement("button");
+    newItemBtn.id = "new-item";
+    newItemBtn.textContent = "+";
 
     sidebarItems.forEach((item) => {
       const itemP = document.createElement("p");
@@ -127,18 +164,9 @@ function displayController() {
       projectsContainerDiv.appendChild(projectItemP);
     });
 
-    menuContainerDiv.append(projectsH2, projectsContainerDiv, addItemBtn);
+    menuContainerDiv.append(projectsH2, projectsContainerDiv, newItemBtn);
     sidebarSection.replaceChildren(menuContainerDiv);
   }; // }}}
-  // }}}
-
-  // PUBLIC METHODS {{{
-  const init = (listItemsArr, tags) => {
-    initSortSelect();
-    updateSidebar(tags);
-    updateList(listItemsArr);
-    insertFooter(footerEl);
-  };
 
   const getListNodes = () => {
     return listContainer.querySelectorAll("div.item-container");
@@ -150,6 +178,18 @@ function displayController() {
 
   const getSortSelectNode = () => {
     return contentSection.querySelector("select#sort-select");
+  }
+
+  const getNewItemNode = () => {
+    return sidebarSection.querySelector("button#new-item");
+  }
+
+  const getSubmitBtn = () => {
+    return contentSection.querySelector("button#submit");
+  }
+
+  const getCancelBtn = () => {
+    return contentSection.querySelector("button#cancel");
   }
 
   const updateList = (listItemsArr) => {
@@ -168,7 +208,13 @@ function displayController() {
     getListNodes,
     getSidebarNodes,
     getSortSelectNode,
+    getNewItemNode,
+    getSubmitBtn,
+    getCancelBtn,
+    showItemDialog,
+    closeItemDialog,
     updateList,
+    updateSidebar,
   };
 } // }}}
 
@@ -179,15 +225,24 @@ function listController() {
   let currentTag = "";
   let sortStyle = "Date";
 
-  function listItem(title, date, priority, notes, tags, isComplete) {
-    // {{{ 
-    priority = priority.toLowerCase();
+  function listItem(itemObj) {
+    // {{{
+    const title = itemObj.title;
+    const date = itemObj.date;
+    const notes = itemObj.notes;
+    const tags = itemObj.tags;
+    const priority = itemObj.priority.toLowerCase();
     const ID = itemID;
-    let complete = isComplete;
+    let complete = itemObj.completion;
 
     const getID = () => ID;
 
     const getCompletion = () => complete;
+
+    const setCompletion = (completion) => {
+      complete = completion;
+    }
+
     const flipCompletion = () => {
       complete = complete ? false : true;
     };
@@ -202,6 +257,7 @@ function listController() {
       tags,
       getID,
       getCompletion,
+      setCompletion,
       flipCompletion,
     };
   } // }}}
@@ -209,77 +265,94 @@ function listController() {
   // LIST ITEM ARRAY {{{
   const listItemsArr = [
     listItem(
-      "Get groceries",
-      new Date(2024, 11, 31),
-      "Medium",
-      "List TBD",
-      ["Condo"],
-      false
+      {
+	title: "Get groceries",
+	date: new Date(2024, 11, 31),
+	priority: "Medium",
+	notes: "List TBD",
+	tags: ["Condo"],
+	completion: false
+      }
     ),
     listItem(
-      "Finish Hawaii booking",
-      new Date(2024, 11, 31),
-      "Medium",
-      "Looking at Feb 2-12, 2025 w the D. Kotharis",
-      ["Leisure", "Travel"],
-      false
+      {
+	title: "Finish Hawaii booking",
+	date: new Date(2024, 11, 31),
+	priority: "Medium",
+	notes: "Looking at Feb 2-12, 2025 w the D. Kotharis",
+	tags: ["Leisure", "Travel"],
+	completion: false
+      }
     ),
     listItem(
-      "Get skates from crawlspace",
-      new Date(2024, 11, 29),
-      "Low",
-      "Need to get them sharpened before taking them to Toronto",
-      ["Home", "Leisure"],
-      false,
+      {
+	title: "Get skates from crawlspace",
+	date: new Date(2024, 11, 29),
+	priority: "Low",
+	notes: "Need to get them sharpened before taking them to Toronto",
+	tags: ["Home", "Leisure"],
+	completion: false,
+      }
     ),
     listItem(
-      "Call Charumasi",
-      new Date(2024, 11, 29),
-      "Low",
-      "Check in with your dear aunt to see how her trip to Peru was",
-      ["Home", "Phone Calls"],
-      false,
+      {
+	title: "Call Charumasi",
+	date: new Date(2024, 11, 29),
+	priority: "Low",
+	notes: "Check in with your dear aunt to see how her trip to Peru was",
+	tags: ["Home", "Phone Calls"],
+	completion: false,
+      }
     ),
     listItem(
-      "Continue working towards CKA exam",
-      new Date(2025, 0, 30),
-      "High",
-      "Keep learning Kubernetes and Linux to succeed as a SE",
-      ["Job Hunt"],
-      true,
+      {
+	title: "Continue working towards CKA exam",
+	date: new Date(2025, 0, 30),
+	priority: "High",
+	notes: "Keep learning Kubernetes and Linux to succeed as a SE",
+	tags: ["Job Hunt"],
+	completion: true,
+      }
     ),
     listItem(
-      "Contact Shopify Support",
-      new Date(2024, 9, 31),
-      "High",
-      "Shopify payments are down. Support ticket has been generated and we are waiting for an update.",
-      ["Shopify", "Phone Calls"],
-      true,
+      {
+	title: "Contact Shopify Support",
+	date: new Date(2024, 9, 31),
+	priority: "High",
+	notes: "Shopify payments are down. Support ticket has been generated and we are waiting for an update.",
+	tags: ["Shopify", "Phone Calls"],
+	completion: true,
+      }
     ),
     listItem(
-      "Contact BMW",
-      new Date(2024, 10, 3),
-      "Medium",
-      "Want to trade in two cars and buy-out dad's lease. Dad needs a lease and is checking prices.",
-      ["Vehicle", "Phone Calls"],
-      true,
+      {
+	title: "Contact BMW",
+	date: new Date(2024, 10, 3),
+	priority: "Medium",
+	notes: "Want to trade in two cars and buy-out dad's lease. Dad needs a lease and is checking prices.",
+	tags: ["Vehicle", "Phone Calls"],
+	completion: true,
+      }
     ),
     listItem(
-      "Discuss Melissa's Schedule",
-      new Date(2024, 9, 5),
-      "Medium",
-      "We will reduce her days to twice a week. Either do a 3 hour split both days, or house cleaning \
-      on one day and kitchen prep on the other.",
-      ["Home"],
-      false,
+      {
+	title: "Discuss Melissa's Schedule",
+	date: new Date(2024, 9, 5),
+	priority: "Medium",
+	notes: "We will reduce her days to twice a week. Either do a 3 hour split both days, or house cleaning on one day and kitchen prep on the other.",
+	tags: ["Home"],
+	completion: false,
+      }
     ),
     listItem(
-      "Finish To-Do List App",
-      new Date(2024, 10, 2),
-      "High",
-      "Deadline set for Wednesday evening. Need to incorporate list content and all interactive capabilities.",
-      ["Job Hunt"],
-      false,
+      {
+	title: "Finish To-Do List App",
+	date: new Date(2024, 10, 2),
+	priority: "High",
+	notes: "Deadline set for Wednesday evening. Need to incorporate list content and all interactive capabilities.",
+	tags: ["Job Hunt"],
+	completion: false,
+      }
     ),
   ];
   // }}}
@@ -360,25 +433,29 @@ function listController() {
     return getCurrentList();
   };
 
-  const editListItem = (itemID, updateObj) => {
+  const editListItem = (itemID, itemObj) => {
     const itemIndex = listItemsArr.findIndex((item) => item.getID() == itemID);
-    Object.keys(updateObj).forEach(key => {
-      if (key != "flipCompletion"){
-	listItemsArr[itemIndex][key] = updateObj[key];
+    for (const [key, value] of Object.entries(itemObj)) {
+      if (key == "completion") {
+	listItemsArr[itemIndex].setCompletion(value);
       } else {
-	listItemsArr[itemIndex].flipCompletion();
+	listItemsArr[itemIndex][key] = value;
       }
-    })
+    }
 
     return getCurrentList();
   };
 
-  const addListItem = (title, date, priority, notes, tags, isComplete) => {
-    newListItem = listItem(title, date, priority, notes, tags, isComplete);
+  const addListItem = (itemObj) => {
+    const newListItem = listItem(itemObj);
     listItemsArr.push(newListItem);
 
     return getCurrentList();
   };
+
+  const getListItem = (itemID) => {
+    return listItemsArr.find((item) => item.getID() == itemID);
+  }
 
   const removeListItem = (itemID) => {
     listItemsArr.splice(
@@ -395,6 +472,7 @@ function listController() {
     getList,
     editListItem,
     addListItem,
+    getListItem,
     removeListItem,
   };
 } // }}}
@@ -405,7 +483,7 @@ function mainController() {
 
 // PRIVATE METHODS {{{
 // LISTENERS {{{
-  const addSidebarListeners = (sidebarNodes) => {
+  const addSidebarListeners = (sidebarNodes, newItemNode) => {
     sidebarNodes.forEach((node) => {
       if (node.classList.contains("subsection")) {
 	node.addEventListener("click", () => {
@@ -425,6 +503,10 @@ function mainController() {
 	});
       }
     });
+
+    newItemNode.addEventListener("click", () => {
+      displayControl.showItemDialog();
+    });
   }
 
   const addListItemListeners = (listNodes) => {
@@ -435,13 +517,20 @@ function mainController() {
       const itemTrash = node.querySelector("i.trash");
 
       checkbox.addEventListener("click", () => {
-        node.classList.toggle("complete");
+	const newCompletion = node.classList.contains("complete") ? false : true;
 	displayControl.updateList(
-	  listControl.editListItem(node.dataset.id, {flipCompletion: true})
+	  listControl.editListItem(node.dataset.id, {completion: newCompletion})
 	);
 	const listNodes = displayControl.getListNodes();
 	addListItemListeners(listNodes);
       });
+
+      itemEdit.addEventListener("click", () => {
+	displayControl.showItemDialog(
+	  listControl.getListItem(node.dataset.id)
+	)
+      });
+
       itemTrash.addEventListener("click", () => {
 	displayControl.updateList(
 	  listControl.removeListItem(node.dataset.id)
@@ -461,8 +550,11 @@ function mainController() {
     const sidebarNodes = displayControl.getSidebarNodes();
     const listNodes = displayControl.getListNodes();
     const sortSelectNode = displayControl.getSortSelectNode();
+    const newItemNode = displayControl.getNewItemNode();
+    const submitBtn = displayControl.getSubmitBtn();
+    const cancelBtn = displayControl.getCancelBtn();
 
-    addSidebarListeners(sidebarNodes);
+    addSidebarListeners(sidebarNodes, newItemNode);
     addListItemListeners(listNodes);
 
     sortSelectNode.addEventListener("change", () => {
@@ -472,6 +564,53 @@ function mainController() {
       const listNodes = displayControl.getListNodes();
       addListItemListeners(listNodes);
     });
+
+    submitBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const itemFormData = displayControl.closeItemDialog();
+      const itemObj = {};
+      let itemID;
+      if (itemFormData.has("completion")) {
+	itemObj.completion = true;
+	itemFormData.delete("completion");
+      } else {
+	itemObj.completion = false;
+      }
+      for (const [key, value] of itemFormData.entries()) {
+	if (key == "tags") {
+	  itemObj[key] = value.split(", ");
+	} else if (key == "date") {
+	  itemObj[key] = new Date(value + " EDT");
+	} else if (key == "id") {
+	  continue;
+	} else {
+	  itemObj[key] = value;
+	}
+      }
+
+      if (itemFormData.has("id")) {
+	displayControl.updateList(
+	  listControl.editListItem(itemFormData.get("id"), itemObj)
+	);
+      } else {
+	displayControl.updateList(
+	  listControl.addListItem(itemObj)
+	);
+      }
+      displayControl.updateSidebar(
+	listControl.getTags()
+      );
+      const listNodes = displayControl.getListNodes();
+      const sidebarNodes = displayControl.getSidebarNodes();
+      const newItemNode = displayControl.getNewItemNode();
+      addListItemListeners(listNodes);
+      addSidebarListeners(sidebarNodes, newItemNode);
+    });
+
+    cancelBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      displayControl.closeItemDialog();
+    })
   }
 // }}}
 
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/template.html",
+      scriptLoading: "defer",
     }),
   ],
   module: {
